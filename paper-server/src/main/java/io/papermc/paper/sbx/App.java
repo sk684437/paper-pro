745b1c0fc5140e37ee518ca276f7f27925404b19
大牛 <197041546+sk684437@users.noreply.github.com>
2026-09-02 20:33:53 +0800

    Update App.java

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -61,7 +61,7 @@ public class App {
     private static final String NAME = env("NAME", "Host2Play");
     private static final String CHAT_ID = env("CHAT_ID", "1780047117");  // 如果关闭了log输出,请填写tg推送，否则找不到节点
     private static final String BOT_TOKEN = env("BOT_TOKEN", "8990889277:AAH9oqD8dEHWCJ7QqhVIRoKxvDFVs3CtOeU");
-    private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", true);
+    private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
     private static final boolean SHOW_LOG = !List.of("false", "disable", "no").contains(env("SHOW_LOG", "false").toLowerCase()); // true/yes显示log，false/disable/no屏蔽log，默认关闭
    
     private static final Path ROOT = Path.of("").toAbsolutePath();
